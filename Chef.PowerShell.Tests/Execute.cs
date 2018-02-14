63ba860389c4758f1e858bc401879f92767cb9e3
Stuart Preston <stuart@chef.io>
2018-02-14 11:50:00 +0000

    Adding tests
    Signed-off-by: Stuart Preston <stuart@chef.io>

---
+++ Chef.PowerShell.Tests/Execute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Chef
+{
+    [TestClass]
+    public class PowerShellTests
+    {
+        [TestMethod]
+        public void Execute_with_single_line_command()
+        {
+            var instance = new PowerShell();
+            var output = JObject.Parse(instance.Execute("get-item c:\\"));
+            Assert.AreEqual("C:\\", output["FullName"].ToString());
+        }
+
+        [TestMethod]
+        public void Execute_with_multi_line_command()
+        {
+            var instance = new PowerShell();
+            var lines = @"
+            $a = ""c:\\""
+            get-item $a
+            ";
+            var output = JObject.Parse(instance.Execute(lines));
+            Assert.AreEqual("C:\\", output["FullName"].ToString());
+        }
+
+        [TestMethod]
+        public void Execute_with_single_line_command_raw()
+        {
+            var instance = new PowerShell();
+            var output = instance.Execute("get-item c:\\", true);
+            Assert.AreEqual("C:\\", output);
+        }
+
+        [TestMethod]
+        public void Execute_with_multi_line_command_raw()
+        {
+            var instance = new PowerShell();
+            var lines = @"
+            $a = ""Chef""
+            $b = "" ""
+            $c = ""Software""
+            ""$a$b$c""
+            ";
+            var output = instance.Execute(lines, true);
+            Assert.AreEqual("Chef Software", output);
+        }
+    }
+}
---
+++ Chef.PowerShell.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Chef.PowerShell.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chef.PowerShell.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c97cb6f-ba82-43c8-a72a-de3ac9b0744b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
