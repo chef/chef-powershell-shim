5cfa838d36af37198697306b7de5a109345380a2
Stuart Preston <stuart@chef.io>
2018-06-12 20:58:24 +0100

    Adding Managed wrapper
    Signed-off-by: Stuart Preston <stuart@chef.io>

--- Chef.PowerShell/Properties/AssemblyInfo.cs
+++ Chef.PowerShell/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Chef.PowerShell")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Chef Software 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
