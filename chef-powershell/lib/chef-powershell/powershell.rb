84a8bcbcbd5eafcc88786f1ab927ecbba6244c8e
Neha Pansare <neha.pansare@progress.com>
2022-08-03 12:21:10 +0530

    Update debugging loggers
    Signed-off-by: Neha Pansare <neha.pansare@progress.com>

--- chef-powershell/lib/chef-powershell/powershell.rb
+++ chef-powershell/lib/chef-powershell/powershell.rb
@@ -91,15 +91,13 @@ class ChefPowerShell
     private
 
     def exec(script, timeout: -1)
-      puts "===== in PowerShell#exec script: #{script.inspect}"
       timeout = -1 if timeout == 0 || timeout.nil?
       PowerMod.set_ps_dll(@powershell_dll)
       PowerMod.set_ps_timeout(timeout)
       PowerMod.set_ps_command(script)
       execution = PowerMod.do_work
-      puts "======= in PowerShell#exec execution: #{execution.inspect}"
       output = execution.read_utf16string
-      puts "======= in PowerShell#exec output: #{output.inspect}"
+      puts "======= in PowerShell#exec for script: #{script} output: #{output.inspect}"
       begin
         hashed_outcome = FFI_Yajl::Parser.parse(output)
         puts "======= in PowerShell#exec hashed_outcome: #{hashed_outcome.inspect}"
