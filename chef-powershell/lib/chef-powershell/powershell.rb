359a868215ff11ec92aa008cab806f3df6d28ec4
Thomas Powell <powell@progress.com>
2023-03-07 11:40:28 -0500

    Use instance var @last_output instead of nonexistent output
    Signed-off-by: Thomas Powell <powell@progress.com>

--- chef-powershell/lib/chef-powershell/powershell.rb
+++ chef-powershell/lib/chef-powershell/powershell.rb
@@ -100,7 +100,7 @@ class ChefPowerShell
       # the data is being prematurely marked for garbage collection
       @last_execution = PowerMod.do_work
       @last_output = @last_execution.read_utf16string
-      hashed_outcome = FFI_Yajl::Parser.parse(output)
+      hashed_outcome = FFI_Yajl::Parser.parse(@last_output)
       @result = FFI_Yajl::Parser.parse(hashed_outcome["result"])
       @errors = hashed_outcome["errors"]
       @verbose = hashed_outcome["verbose"]
