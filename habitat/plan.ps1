ff9e8a0cf6f9f0f4fee980f4f07fa623d46b9e7f
John McCrae <john.mccrae@progress.com>
2024-08-19 06:48:48 -0700

    updating framework versions
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,14 +1,14 @@
 $pkg_name="chef-powershell-shim"
 $pkg_origin="chef"
-$pkg_version="0.3.3"
+$pkg_version="0.4.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
 $pkg_build_deps=@(
   "core/nuget",
-  "core/dotnet-45-dev-pack",
+  # "core/dotnet-481-dev-pack", As of August 2024, this pacakge should be installed by default on all Windows devices.
   "core/windows-10-sdk",
-  "core/visual-build-tools-2019",
-  "core/dotnet-5-sdk"
+  "core/visual-build-tools-2022",
+  "core/dotnet-8-sdk"
 )
 $pkg_bin_dirs=@("bin")
 
@@ -33,14 +33,14 @@ function Invoke-Build {
 
 function Invoke-Install {
   Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/Chef.Powershell.Wrapper/x64/release/*.dll "$pkg_prefix/bin"
-  Copy-Item "$env:VCToolsInstallDir_160\x64\Microsoft.VC142.CRT\*.dll" "$pkg_prefix/bin"
+  Copy-Item "$env:VCToolsInstallDir_160\x64\Microsoft.VC143.CRT\*.dll" "$pkg_prefix/bin"
 
-  dotnet publish --output $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0 --self-contained --configuration Release --runtime win10-x64 $HAB_CACHE_SRC_PATH/$pkg_dirname/Chef.Powershell.Core/Chef.Powershell.Core.csproj
+  dotnet publish --output $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0 --self-contained --configuration Release --runtime win10-x64 $HAB_CACHE_SRC_PATH/$pkg_dirname/Chef.Powershell.Core/Chef.Powershell.Core.csproj
 
-  Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/Chef.Powershell.Wrapper.Core/x64/release/*.dll $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0
-  Copy-Item $PLAN_CONTEXT/../Chef.PowerShell.Wrapper.Core/Chef.PowerShell.Wrapper.Core.runtimeconfig.json $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0/Chef.Powershell.Wrapper.Core.runtimeconfig.json
-  Rename-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0/Chef.Powershell.Core.deps.json $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0/Microsoft.NETCore.App.deps.json
-  mkdir $pkg_prefix/bin/host/fxr/5.0.0
-  Copy-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0/hostfxr.dll $pkg_prefix/bin/host/fxr/5.0.0
-  Copy-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/5.0.0/Ijwhost.dll $pkg_prefix/bin
+  Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/Chef.Powershell.Wrapper.Core/x64/release/*.dll $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0
+  Copy-Item $PLAN_CONTEXT/../Chef.PowerShell.Wrapper.Core/Chef.PowerShell.Wrapper.Core.runtimeconfig.json $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0/Chef.Powershell.Wrapper.Core.runtimeconfig.json
+  Rename-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0/Chef.Powershell.Core.deps.json $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0/Microsoft.NETCore.App.deps.json
+  mkdir $pkg_prefix/bin/host/fxr/8.0.0
+  Copy-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0/hostfxr.dll $pkg_prefix/bin/host/fxr/8.0.0
+  Copy-Item $pkg_prefix/bin/shared/Microsoft.NETCore.App/8.0.0/Ijwhost.dll $pkg_prefix/bin
 }
