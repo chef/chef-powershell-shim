8f5c30b59f383d3bdfbae585276f9f0ff80464c3
John <john.mccrae@progress.com>
2024-08-20 22:01:19 +0000

    Updating the build tools for the PowerShell Shim
    Signed-off-by: John <john.mccrae@progress.com>

--- habitat-dotnet-481-dev-pack-x64/plan.ps1
+++ habitat-dotnet-481-dev-pack-x64/plan.ps1
@@ -5,8 +5,9 @@ $pkg_description=".net framework 4.8.1 with dev pack"
 $pkg_upstream_url="https://www.microsoft.com/net/download/all"
 $pkg_license=@("Microsoft Software License")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://dotnet.microsoft.com/en-us/download/dotnet-framework/thank-you/net481-developer-pack-offline-installer"
-$pkg_shasum="56e3dc8b9013e0af1f62bc7499395726c330e35265db870f5cbd51b5ad841bb7"
+$pkg_source="https://go.microsoft.com/fwlink/?linkid=2203306"
+$pkg_filename="NDP481-DevPack-ENU.exe"
+$pkg_shasum="fd9ae4b63cbc8f7637249c4b29d25281b1f70fbe6d650109dcbf2bdf596cf994"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools\x64")
 $pkg_lib_dirs=@("Program Files (x86)\Windows Kits\NETFXSDK\4.8.1\Lib\um\x64")
@@ -18,7 +19,7 @@ function Invoke-SetupEnvironment {
 
 function Invoke-Unpack {
     Start-Process "$HAB_CACHE_SRC_PATH/$pkg_filename" -Wait -ArgumentList "/features OptionId.NetFxSoftwareDevelopmentKit /layout $HAB_CACHE_SRC_PATH/$pkg_dirname /quiet"
-    Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname/Redistributable/4.5.50710"
+    Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
     try {
         Get-ChildItem "*.msi" | ForEach-Object {
             lessmsi x $_
@@ -30,5 +31,5 @@ function Invoke-Install {
     Get-ChildItem "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Include "Program Files" -Recurse | ForEach-Object {
         Copy-Item $_ "$pkg_prefix" -Recurse -Force
     }
-    Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/Redistributable/4.5.50710/netfx45_dtp/SourceDir/ProgramFilesFolder/*" "$pkg_prefix/Program Files" -Recurse
+    # Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/Redistributable/4.5.50710/netfx45_dtp/SourceDir/ProgramFilesFolder/*" "$pkg_prefix/Program Files" -Recurse
 }
