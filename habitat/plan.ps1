8ce40164d48842aa56f06cf1f89a3d46e5a10eea
John McCrae <john.mccrae@progress.com>
2025-10-27 15:15:57 -0700

    tweaking the build env
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,7 +1,7 @@
 $env:HAB_BLDR_CHANNEL="base-2025"
 $env:HAB_REFRESH_CHANNEL="base-2025"
 $env:MSBuildEnableWorkloadResolver = $false
-$envL:HAB_ORIGIN="chef"
+$env:HAB_ORIGIN="chef"
 $pkg_name="chef-powershell-shim"
 $pkg_origin="chef"
 $pkg_version="18.6.3"
