9837fd56eb05c397333bac30d56621ef6fee6a8b
John McCrae <john.mccrae@progress.com>
2026-08-03 12:33:18 -0700

    Still have weird gem uri issues, not deriving ruby version another way
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/build_gems.ps1
+++ .expeditor/build_gems.ps1
@@ -155,9 +155,9 @@ Write-Output "`r"
 
 Write-Output "--- :building_construction: Setting up Environment Variables for Ruby and Chef PowerShell"
 $temp = Get-Location
-$gem_path = [string]$temp.path + "\vendor\bundle\ruby\3.4.0"
+$ruby_version = (ruby -e "puts RbConfig::CONFIG['ruby_version']").Trim()
+$gem_path = [string]$temp.path + "\vendor\bundle\ruby\$ruby_version"
 [Environment]::SetEnvironmentVariable("GEM_PATH", $gem_path)
-[Environment]::SetEnvironmentVariable("GEM_ROOT", $gem_path)
 [Environment]::SetEnvironmentVariable("BUNDLE_GEMFILE", "$($temp.path)\Gemfile")
 Write-Output "`r"
 
